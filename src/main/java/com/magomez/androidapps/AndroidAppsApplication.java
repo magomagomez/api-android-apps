eb7b409cc8ab6fe4dcc688e253fe37c648be5267
Magomez <magomagomez@gmail.com>
2025-04-16 10:53:46 +0200

    fix twilio xml response

--- src/main/java/com/magomez/androidapps/AndroidAppsApplication.java
+++ src/main/java/com/magomez/androidapps/AndroidAppsApplication.java
@@ -1,12 +1,17 @@
 package com.magomez.androidapps;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableAutoConfiguration
@@ -19,6 +24,15 @@ public class AndroidAppsApplication {
     public static void main(String... args) {
         new SpringApplicationBuilder(AndroidAppsApplication.class)
                 .run(args);
+    }
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void configureContentNegotiation(@NotNull ContentNegotiationConfigurer configurer) {
+                configurer.defaultContentType(MediaType.APPLICATION_JSON);
+            }
+        };
     }
 
 }
